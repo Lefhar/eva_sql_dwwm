54528d763ee8ef7c8bd48bdb2cc48392b84f68b1
harold <s.lefebvre907@laposte.net>
2021-03-05 12:33:10 +0100

    evaluation sql dwwm

---
+++ 5-roles/roles.sql
@@ -0,0 +1,21 @@
+--création des utilisateurs
+
+CREATE USER 'paul'@'%' IDENTIFIED BY 'mdp';
+CREATE USER 'gertrude'@'%' IDENTIFIED BY 'mdp';
+CREATE USER 'marion'@'%' IDENTIFIED BY 'mdp';
+CREATE USER 'fode'@'%' IDENTIFIED BY 'mdp';
+
+--création du groupe marketing
+
+CREATE ROLE marketing;
+
+--attribution des roles par base
+GRANT SELECT, UPDATE, DELETE, INSERT ON categories.gescom_afpa TO marketing
+GRANT SELECT, UPDATE, DELETE, INSERT ON gescom_afpa.orders TO marketing
+GRANT SELECT, UPDATE, DELETE, INSERT ON gescom_afpa.orders_details TO marketing  
+GRANT SELECT, UPDATE, DELETE, INSERT ON gescom_afpa.products TO marketing  
+GRANT SELECT, UPDATE, DELETE, INSERT ON gescom_afpa.customers TO marketing
+
+
+--attribution du role au utilisateur
+GRANT marketing TO paul, gertrude, marion, fode;
